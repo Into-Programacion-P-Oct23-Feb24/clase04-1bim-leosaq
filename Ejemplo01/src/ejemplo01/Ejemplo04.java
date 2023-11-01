3e7d30e19a01b7a8886f828b8fa673b7c6a9440b
leosaq <leandrosaquisari7@gmail.com>
2023-11-01 15:41:04 -0500

    printlnvsprint

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -11,6 +11,12 @@ package ejemplo01;
  */
 public class Ejemplo04 {
     public static void main(String[] args) {
+        /*
+        println vs print
+        La diferencia es que printl sirve para separar en diferentes lineas su contenido 
+        mientras que print ejecuta todo en una sola linea y se puede escribir en diferentes lineas 
+        en las cuales como resultado final se va a juntar todo en una sola linea
+        */
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
